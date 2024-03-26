31e0dc6957a3b47ed950141e0316c28f8ce597b8
Williams Amaya <whistemalo@gmail.com>
2024-03-26 17:34:51 -0600

    categories

--- middleware.ts
+++ middleware.ts
@@ -18,17 +18,17 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) return;
-  if (isAuthRoute){
-    if (isLoggedIn){
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    }
-    return;
-  }
-  if(!isLoggedIn && !isPublicRoute){
-    return Response.redirect(new URL("/auth/login", nextUrl))
-  }
-  return;
+  // if (isApiAuthRoute) return;
+  // if (isAuthRoute){
+  //   if (isLoggedIn){
+  //     return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  //   }
+  //   return;
+  // }
+  // if(!isLoggedIn && !isPublicRoute){
+  //   return Response.redirect(new URL("/auth/login", nextUrl))
+  // }
+  // return;
 })
 
 export const config = {
